ccee3c11695fae38a858798a1cb9f11412a84ac0
TheAdnan <adnankicin92@gmail.com>
2017-02-23 00:42:02 +0100

    quora nightly

---
+++ nightly.js
@@ -0,0 +1,8 @@
+function nightly(){
+	var css = document.createElement("style");
+	css.type = "text/css";
+	css.innerHTML = "* { background: rgba(0,0,0, 0.8)!important; color: rgba(255,255,255, 0.6)!important;}";
+	document.getElementsByTagName("head")[0].appendChild(css);
+}
+
+nightly();
