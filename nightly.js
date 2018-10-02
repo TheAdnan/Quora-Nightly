fc3d06d80eee5a9ad74137e225e3a922de98704b
Gagan <gagan@Gagans-MacBook-Pro.local>
2018-10-02 18:46:34 +0530

    change color
     Committer: theTechie

--- nightly.js
+++ nightly.js
@@ -1,7 +1,7 @@
-const CSS = "* { background: rgba(41, 0, 4, 0.79)!important; color: rgba(255,255,255, 0.6)!important;}";
-const TOOLTIP_ENABLE = "Enable night mode";
-const TOOLTIP_DISABLE = "Disable night mode";
-const URL = "quora.com";
+const CSS = '* { background: rgb(58, 60, 63)!important; color: rgba(255,255,255, 0.6)!important;}';
+const TOOLTIP_ENABLE = 'Enable night mode';
+const TOOLTIP_DISABLE = 'Disable night mode';
+const URL = 'quora.com';
 
 function enableNightMode(tab, title) {
     browser.pageAction.setTitle({tabId: tab.id, title: TOOLTIP_DISABLE});
@@ -14,28 +14,31 @@ function disableNightMode(tab, title) {
 }
 
 function toggleNightMode(tab) {
-  function gotTitle(title) {
-      if (title === TOOLTIP_ENABLE) {
-        enableNightMode(tab, title);
-        browser.storage.local.set({nightModeEnabled:true})
-    } else {
-        disableNightMode(tab, title);
-        browser.storage.local.set({nightModeEnabled:false})
+    function gotTitle(title) {
+        if (title === TOOLTIP_ENABLE) {
+            enableNightMode(tab, title);
+            browser.storage.local.set({nightModeEnabled: true});
+        } else {
+            disableNightMode(tab, title);
+            browser.storage.local.set({nightModeEnabled: false});
+        }
     }
-  }
 
-  var gettingTitle = browser.pageAction.getTitle({tabId: tab.id});
-  gettingTitle.then(gotTitle);
+    var gettingTitle = browser.pageAction.getTitle({tabId: tab.id});
+    gettingTitle.then(gotTitle);
 }
 
 function initializePageAction(tab) {
     var thisTab = tab;
-    if(tab.url.indexOf(URL) > -1) {
-        browser.pageAction.setTitle({tabId: tab.id, title: TOOLTIP_ENABLE});
+    if (tab.url.indexOf(URL) > -1) {
+        browser.pageAction.setTitle({
+            tabId: tab.id,
+            title: TOOLTIP_ENABLE
+        });
         browser.pageAction.show(tab.id);
 
         function onGot(item) {
-            if(item["nightModeEnabled"]) {
+            if (item['nightModeEnabled']) {
                 enableNightMode(thisTab);
             }
         }
@@ -44,20 +47,20 @@ function initializePageAction(tab) {
             console.log(`Error: ${error}`);
         }
 
-        var nightModeEnabled = browser.storage.local.get("nightModeEnabled");
+        var nightModeEnabled = browser.storage.local.get('nightModeEnabled');
         nightModeEnabled.then(onGot, onError);
     }
 }
 
 var gettingAllTabs = browser.tabs.query({});
-gettingAllTabs.then((tabs) => {
-  for (let tab of tabs) {
-    initializePageAction(tab);
-  }
+gettingAllTabs.then(tabs => {
+    for (let tab of tabs) {
+        initializePageAction(tab);
+    }
 });
 
 browser.tabs.onUpdated.addListener((id, changeInfo, tab) => {
-  initializePageAction(tab);
+    initializePageAction(tab);
 });
 
 browser.pageAction.onClicked.addListener(toggleNightMode);
